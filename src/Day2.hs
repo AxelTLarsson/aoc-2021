4461e14e86b5048a9e4ac1d76bd33beff2a80ad1
AxelTLarsson <mail@axellarsson.nu>
2021-12-02 22:54:21 +0100

    🌟 Solve Day 2 part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -14,21 +14,32 @@ solve :: IO ()
 solve = do
     parsed <- fmap parse (input "data/day2_input.txt")
     -- print $ parsed
-    pos <- return (run parsed)
+    pos <- return (run2 parsed)
     print pos
-    print (fst pos * snd pos)
+    print (result pos)
 
+result :: (Horizontal, Depth, Aim) -> Int
+result (h, d, a) = h * d
 
 run :: [Command] -> (Horizontal, Depth)
 run cmds = foldl (runCmd) (0, 0) cmds
 
+run2 :: [Command] -> (Horizontal, Depth, Aim)
+run2 cmds = foldl (runCmd2) (0, 0, 0) cmds
+
 type Horizontal = Int
 type Depth = Int
+type Aim = Int
 
 runCmd :: (Horizontal, Depth) -> Command -> (Horizontal, Depth)
 runCmd (h, d) (Forward x) = (h + x, d)
 runCmd (h, d) (Up x)      = (h, d - x)
 runCmd (h, d) (Down x)    = (h, d + x)
+
+runCmd2 :: (Horizontal, Depth, Aim) -> Command -> (Horizontal, Depth, Aim)
+runCmd2 (h, d, a) (Forward x) = (h + x, d + a * x, a)
+runCmd2 (h, d, a) (Up x)      = (h, d, a - x)
+runCmd2 (h, d, a) (Down x)    = (h, d, a + x)
 
 parse :: [String] -> [Command]
 parse x = map (fst . head) $ map (readP_to_S commandParser) x
@@ -67,17 +78,3 @@ up = do
     return $ Up units
 
 data Command = Forward Int | Down Int | Up Int deriving (Show)
-
-{-
- - solve = do
- -   input <- readFile "data/day2_full.txt"
- -   let parsed = parse $ lines input
- -   let valids = filter validPassword2 parsed
- -   print $ length valids
- - 
- - parse :: [String] -> [Password]
- - parse ls = map (fst . head) $ map (readP_to_S parser) ls
- - 
- - digit :: ReadP Char
- - digit = satisfy (\char -> char >= '0' && char <= '9')
- -}
